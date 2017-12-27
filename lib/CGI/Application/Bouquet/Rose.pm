5c9e463d2606824cd9bcc3f0bf486b8f4ccf214c
ronsavage <ron@savage.net.au>
2017-12-27 16:16:22 +1100

    Start modernizing the distro

--- lib/CGI/Application/Bouquet/Rose.pm
+++ lib/CGI/Application/Bouquet/Rose.pm
@@ -20,25 +20,6 @@ use File::Path; # For mkpath and rmtree.
 use File::Spec; # For copy.
 use HTML::Template;
 
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use CGI::Application::Bouquet::Rose ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-
-);
-
 our $VERSION = '1.06';
 
 # -----------------------------------------------
